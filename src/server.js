9ae94363f33fb8c61e61dfcac0536b9dfc9315dd
Florian Hämmerle <haemmerle.florian@googlemail.com>
2016-11-01 21:59:43 +0100

    add /auth/register route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const Hapi = require('hapi')
 const env = require('./env')
 const schemas = require('./helpers/schemas')
 const validate = require('./helpers/validate')
+const userRepository = require('./services/storage/repositories/user')
 
 const server = new Hapi.Server()
 server.connection({ port: env.PORT })
@@ -15,6 +16,25 @@ server.route({
   handler: (request, reply) => {
     validate(request.payload, schemas.track).then(track => {
       reply(track).code(201)
+    }).catch(err => {
+      reply({ error: true }).code(400)
+    })
+  }
+})
+
+/**
+ * Register a new user.
+ */
+server.route({
+  method: 'POST',
+  path: '/auth/register',
+  handler: (request, reply) => {
+    validate(request.payload, schemas.auth).then(user => {
+      userRepository.create(user).then(response => {
+        reply(user).code(201)
+      }).catch(err => {
+        reply({ error: true }).code(500)
+      })
     }).catch(err => {
       reply({ error: true }).code(400)
     })
--- test/server.js
+++ test/server.js
@@ -1,6 +1,9 @@
 const { assert } = require('chai')
+const sinon = require('sinon')
 const server = require('../src/server')
 const track = require('./mocks/track-raw')
+const user = require('./mocks/user-raw')
+const userRepository = require('../src/services/storage/repositories/user')
 
 describe('server', () => {
 
@@ -16,55 +19,138 @@ describe('server', () => {
     server.stop()
   })
 
-  describe('/tracks', () => {
-    describe('POST', () => {
-      context('request succeeds', () => {
-        it('responds with status code 201', (done) => {
-          server.inject({
-            method: 'POST',
-            url: '/tracks',
-            payload: track,
-          }, (response) => {
-            assert.equal(response.statusCode, 201)
-            done()
-          })
-        })
+  describe('POST /auth/register', () => {
 
-        it('returns the newly created track', (done) => {
-          server.inject({
-            method: 'POST',
-            url: '/tracks',
-            payload: track,
-          }, (response) => {
-            const responseObj = JSON.parse(response.payload)
-            assert.deepEqual(responseObj, track)
-            done()
-          })
+    beforeEach(() => {
+      sinon.stub(userRepository, 'create')
+    })
+
+    afterEach(() => {
+      userRepository.create.restore()
+    })
+
+    context('valid data (email, password)', () => {
+      beforeEach(() => {
+        userRepository.create.returns(Promise.resolve(user))
+      })
+
+      it('creates a new user', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/auth/register',
+          payload: user,
+        }, (response) => {
+          assert.isTrue(userRepository.create.calledWithMatch(user))
+          done()
         })
       })
 
-      context('request fails', () => {
-        it('responds with status code 400', (done) => {
-          server.inject({
-            method: 'POST',
-            url: '/tracks',
-            payload: Object.assign({}, track, { owner: undefined }),
-          }, (response) => {
-            assert.equal(response.statusCode, 400)
-            done()
-          })
+      it('responds with 201', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/auth/register',
+          payload: user,
+        }, (response) => {
+          assert.equal(response.statusCode, 201)
+          done()
         })
+      })
 
-        it('returns an error', (done) => {
-          server.inject({
-            method: 'POST',
-            url: '/tracks',
-            payload: Object.assign({}, track, { owner: undefined }),
-          }, (response) => {
-            const responseObj = JSON.parse(response.payload)
-            assert.isObject(responseObj)
-            done()
-          })
+      it('returns the newly created user', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/auth/register',
+          payload: user,
+        }, (response) => {
+          const responseObj = JSON.parse(response.payload)
+          assert.deepEqual(responseObj, user)
+          done()
+        })
+      })
+    })
+
+    context('invalid data', () => {
+      beforeEach(() => {
+        userRepository.create.returns(Promise.resolve(user))
+      })
+
+      it('responds with 400', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/auth/register',
+          payload: Object.assign({}, user, { email: undefined }),
+        }, (response) => {
+          assert.equal(response.statusCode, 400)
+          done()
+        })
+      })
+    })
+
+    context('valid data but storing fails', () => {
+      beforeEach(() => {
+        userRepository.create.returns(Promise.reject(new Error()))
+      })
+
+      it('responds with 500', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/auth/register',
+          payload: user,
+        }, (response) => {
+          assert.equal(response.statusCode, 500)
+          done()
+        })
+      })
+    })
+  })
+
+  describe('POST /tracks', () => {
+    context('valid data', () => {
+      it('responds with status code 201', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/tracks',
+          payload: track,
+        }, (response) => {
+          assert.equal(response.statusCode, 201)
+          done()
+        })
+      })
+
+      it('returns the newly created track', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/tracks',
+          payload: track,
+        }, (response) => {
+          const responseObj = JSON.parse(response.payload)
+          assert.deepEqual(responseObj, track)
+          done()
+        })
+      })
+    })
+
+    context('invalid data', () => {
+      it('responds with status code 400', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/tracks',
+          payload: Object.assign({}, track, { owner: undefined }),
+        }, (response) => {
+          assert.equal(response.statusCode, 400)
+          done()
+        })
+      })
+
+      it('returns an error', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/tracks',
+          payload: Object.assign({}, track, { owner: undefined }),
+        }, (response) => {
+          const responseObj = JSON.parse(response.payload)
+          assert.isObject(responseObj)
+          done()
         })
       })
     })
