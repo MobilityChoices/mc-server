997bd0200438acb84ba2664eef0f7dd3b729ec8e
Florian Hämmerle <haemmerle.florian@googlemail.com>
2016-11-09 19:22:06 +0100

    prepare testing for POST /auth/login (missing password)

--- test/server.ts
+++ test/server.ts
@@ -402,6 +402,26 @@ describe('server', () => {
     })
 
     context('missing password', () => {
+      beforeEach(() => {
+        sinon.stub(userRepository, 'findByEmail')
+        user$findByEmail = userRepository.findByEmail as sinon.SinonStub
+        user$findByEmail.returns(Promise.resolve(userDocument))
+      })
+
+      afterEach(() => {
+        user$findByEmail.restore()
+      })
+
+      const request = {
+        headers: {
+          'content-type': 'application/json',
+        },
+        method: 'POST',
+        url: '/auth/login',
+        payload: {
+          email: 'alpha@beta.gamma',
+        }
+      }
 
     })
 
