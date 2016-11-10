ef8a155e99dd280474086b8f1d9c4def447162b3
Florian Hämmerle <haemmerle.florian@googlemail.com>
2016-11-10 20:08:02 +0100

    add some error types

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -1,14 +1,33 @@
+type ErrorCode = 'BadArgument' | 'ServerError' | 'MalformedValue' | 'Unauthenticated'
+
 export interface Error {
-  code: 'BadArgument' | 'ServerError'
+  code: ErrorCode
   message?: string
   target?: string
   details?: Error[]
 }
 
-export const serverError = () => ({
-  error: {
-    code: 'ServerError',
-    message: 'an unexpected server error occurred.',
-    target: ''
-  }
+export const serverError = (): Error => ({
+  code: 'ServerError',
+  message: 'an unexpected server error occurred.',
+  target: '',
+})
+
+export const badArgumentError = (target = '', message = '', ...details: Error[]): Error => ({
+  code: 'BadArgument',
+  message,
+  target,
+  details,
+})
+
+export const malformedValueError = (target = '', message = ''): Error => ({
+  code: 'MalformedValue',
+  message,
+  target,
+})
+
+export const authenticationError = (): Error => ({
+  code: 'Unauthenticated',
+  message: 'Token is missing or invalid.',
+  target: 'Authorization-Header',
 })
