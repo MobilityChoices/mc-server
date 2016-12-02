ea8143a6013a13e4c3992d696d8d9a2879d834de
Florian Hämmerle <haemmerle.florian@googlemail.com>
2016-12-02 20:38:54 +0100

    sample google directions response

---
+++ test/fixtures/googleRoute.ts
@@ -0,0 +1,136 @@
+// tslint:disable max-line-length
+export default {
+   'geocoded_waypoints' : [
+      {
+         'geocoder_status' : 'OK',
+         'place_id' : 'ChIJMzWau05rm0cRk8VoN0Cud84',
+         'types' : [ 'street_address' ]
+      },
+      {
+         'geocoder_status' : 'OK',
+         'place_id' : 'ChIJ81hwg5gSm0cRNRYNDTLvf1U',
+         'types' : [ 'street_address' ]
+      }
+   ],
+   'routes' : [
+      {
+         'bounds' : {
+            'northeast' : {
+               'lat' : 47.5007663,
+               'lng' : 9.7442405
+            },
+            'southwest' : {
+               'lat' : 47.4124043,
+               'lng' : 9.727896099999999
+            }
+         },
+         'copyrights' : 'Map data ©2016 Google',
+         'legs' : [
+            {
+               'distance' : {
+                  'text' : '10.4 km',
+                  'value' : 10426
+               },
+               'duration' : {
+                  'text' : '2 hours 7 mins',
+                  'value' : 7626
+               },
+               'end_address' : 'Römerstraße 27, 6900 Bregenz, Austria',
+               'end_location' : {
+                  'lat' : 47.5007663,
+                  'lng' : 9.742317999999999
+               },
+               'start_address' : 'Stadtstraße 500, 6850 Dornbirn, Austria',
+               'start_location' : {
+                  'lat' : 47.4124043,
+                  'lng' : 9.7435446
+               },
+               'steps' : [
+                  {
+                     'distance' : {
+                        'text' : '3.4 km',
+                        'value' : 3432
+                     },
+                     'duration' : {
+                        'text' : '41 mins',
+                        'value' : 2457
+                     },
+                     'end_location' : {
+                        'lat' : 47.4424943,
+                        'lng' : 9.735125799999999
+                     },
+                     'html_instructions' : 'Head \u003cb\u003enorth\u003c/b\u003e on \u003cb\u003eStadtstraße\u003c/b\u003e/\u003cb\u003eVorarlberg Str.\u003c/b\u003e/\u003cb\u003eB190\u003c/b\u003e toward \u003cb\u003eEuropapassage\u003c/b\u003e\u003cdiv style=\'font-size:0.9em\'\u003eContinue to follow Vorarlberg Str./B190\u003c/div\u003e',
+                     'polyline' : {
+                        'points' : 'ofk`Hc`nz@YAw@Gc@QWM[Q_@S}@_@c@M_@E]EK?WA]BO@M@o@HMDSDi@Ji@JeAVg@Dm@H}@F_@@m@B_AH{AVg@HaBRMBqBTyDh@cBVYF_@FoBh@m@LqBh@qFvAq@PwA^iBd@yA^uCv@g@LaBb@{Bh@UFQDUFSFKBcAVMBkAZYFWHQDC@mCp@SF}Bj@cJzBiCp@aB`@w@RqBh@eAXmBd@GB]JsBf@}@R{@TkBd@kCp@aDx@qG~AWH[JYHm@HYHg@LsCr@s@RgDx@'
+                     },
+                     'start_location' : {
+                        'lat' : 47.4124043,
+                        'lng' : 9.7435446
+                     },
+                     'travel_mode' : 'WALKING'
+                  },
+                  {
+                     'distance' : {
+                        'text' : '0.3 km',
+                        'value' : 258
+                     },
+                     'duration' : {
+                        'text' : '3 mins',
+                        'value' : 192
+                     },
+                     'end_location' : {
+                        'lat' : 47.4445706,
+                        'lng' : 9.734388899999999
+                     },
+                     'html_instructions' : 'Turn \u003cb\u003eright\u003c/b\u003e toward \u003cb\u003eVorarlberg Str.\u003c/b\u003e/\u003cb\u003eB190\u003c/b\u003e',
+                     'maneuver' : 'turn-right',
+                     'polyline' : {
+                        'points' : 'qbq`Hqklz@CMCE?A?AA?A?A?A?SDc@JQFQ@Q@o@Go@BMDKFU\\KJGHIBI@K@OCILMJMHWHE@A@A@ABCJ?F'
+                     },
+                     'start_location' : {
+                        'lat' : 47.4424943,
+                        'lng' : 9.735125799999999
+                     },
+                     'travel_mode' : 'WALKING'
+                  },
+                  {
+                     'distance' : {
+                        'text' : '6.7 km',
+                        'value' : 6736
+                     },
+                     'duration' : {
+                        'text' : '1 hour 23 mins',
+                        'value' : 4977
+                     },
+                     'end_location' : {
+                        'lat' : 47.5007663,
+                        'lng' : 9.742317999999999
+                     },
+                     'html_instructions' : 'Slight \u003cb\u003eright\u003c/b\u003e onto \u003cb\u003eVorarlberg Str.\u003c/b\u003e/\u003cb\u003eB190\u003c/b\u003e\u003cdiv style=\'font-size:0.9em\'\u003eContinue to follow B190\u003c/div\u003e\u003cdiv style=\'font-size:0.9em\'\u003eDestination will be on the left\u003c/div\u003e',
+                     'maneuver' : 'turn-slight-right',
+                     'polyline' : {
+                        'points' : 'qoq`H}flz@yBj@eDx@}@Tm@Lc@TQDkCp@{Cv@mCp@aCl@k@N_@JKBMDq@PkBd@qCt@}Bl@sD~@yBh@{@TgCp@yBh@[J]H_B`@eDz@aFnAeDx@w@RcAVi@Nc@Jg@PWJUFo@ZONA@a@N?BA@?@?@A@?@A??@A@A??@A?A?A@A?A@A?A?A?A?A?AAA?AAAAA??AA??AAA?AAAcCb@o@J}@LYBI@A?a@BE@S@i@Bq@@}@@_BA[Ac@A_AEy@Ey@E{@Ga@C_@EC?UKy@EyBM}AKiBK}@Gu@Go@EO?UC_@C_@AqCOq@Gw@GiAIi@EoAMe@IUEs@Mq@Oc@OOGICSKMIQKOOMOg@s@KQKSu@sAY]QOQMMIOGOGKC_@EWAI?K?WBi@D]@c@?a@EE?a@Ie@KqA]oA[u@SyBi@SEg@M{@M]EWA{@Ak@@mA?}B@wA?s@A{AAo@?mAAwBAuB?aBB]?_@?u@?i@A_@CYEUESEk@SUMQGOGiCcAc@Ma@Og@MUG{@OYEWAe@AeACsAE_@Ew@E_COw@CI?mEU_@CsBOsAOmDQk@CyAEuCOc@ASCe@EiBO}@E_@Ek@Ck@Ei@Ii@KWE[Ki@Qo@[YO[OSM_@Um@e@AA[YUUg@i@SYMQW_@k@}@u@{A_@aAWu@So@Qi@Qw@S{@Q_Ae@qCEY?CI_@AIScASmAeBcL'
+                     },
+                     'start_location' : {
+                        'lat' : 47.4445706,
+                        'lng' : 9.734388899999999
+                     },
+                     'travel_mode' : 'WALKING'
+                  }
+               ],
+               'traffic_speed_entry' : [],
+               'via_waypoint' : []
+            }
+         ],
+         'overview_polyline' : {
+            'points' : 'ofk`Hc`nz@qAI{@_@{@e@}@_@c@M}@Kc@Am@D}@JuBb@eAVg@DkBPmAD_AH{AViC\\_CX}G`Ay@NaOxDmIvB{KrC{FxAkUzFoR`Fq\\lIu@TgARwKnCGWC?{@Pc@HQ@o@G}@Ha@d@STSD[AWXm@VGXkKhCu@ZgHhB{IzBwD`A}PlE_ZtHkItByBp@o@ZONc@PAHIHKBKCGIAAcCb@mBXgAHuBH}C?_ACoFYaAIYK{Jk@sDUgFY}E_@uBWiASuA_@YKa@Ua@[u@cAWe@u@sAY]c@]]Q[Kw@GU?aAHaA@g@EgAUqIwB{@SyASsACyB@uE@oCCkJCuEBiAEo@K_AYg@U_FiB}@UuAUwEMwAKwDSwEUsCSsAOmDQeCIyDQy@IsF_@uAOaAQeA]iAk@o@]mA{@{A{Aa@k@cA}Au@{A_@aAk@eBc@aBe@{Bu@oEoC_Q'
+         },
+         'summary' : 'Vorarlberg Str./B190 and B190',
+         'warnings' : [
+            'Walking directions are in beta.    Use caution – This route may be missing sidewalks or pedestrian paths.'
+         ],
+         'waypoint_order' : []
+      }
+   ],
+   'status' : 'OK'
+}
