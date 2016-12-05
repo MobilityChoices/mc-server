b56903871ca8513d0e1b37a540de714119c94b97
Florian Hämmerle <haemmerle.florian@googlemail.com>
2016-12-05 12:39:29 +0100

    google route -> generic route mapping

--- src/helpers/route-helpers.ts
+++ src/helpers/route-helpers.ts
@@ -1,4 +1,36 @@
-import { Route as GoogleRoute, TravelMode } from '../services/google/directions/types'
+import { Route as GoogleRoute, TravelMode, Leg, Step } from '../services/google/directions/types'
+
+type GenericStep = {
+  distance: number,
+  duration: number,
+  start: {
+    address: string | undefined,
+    location: { lat: number, lng: number },
+  },
+  end: {
+    address: string | undefined,
+    location: { lat: number, lng: number },
+  },
+  steps: GenericStep[],
+  travelMode: TravelMode | undefined
+}
+
+function stepMapper(step: Step | Leg): GenericStep {
+  return {
+    distance: step.distance.value,
+    duration: step.duration.value,
+    start: {
+      address: (step as Leg).start_address || undefined,
+      location: step.start_location,
+    },
+    end: {
+      address: (step as Leg).start_address || undefined,
+      location: step.end_location,
+    },
+    steps: (step.steps || []).map(stepMapper),
+    travelMode: ((step as Step).travel_mode || '').toLowerCase() as TravelMode || undefined,
+  }
+}
 
 export function googleToGeneric(route: GoogleRoute | undefined) {
   if (!route) {
@@ -15,5 +47,6 @@ export function googleToGeneric(route: GoogleRoute | undefined) {
       }
     })
   })
-  return { ...route, travelMode }
+  const legs = route.legs.map(stepMapper)
+  return { legs, travelMode }
 }
