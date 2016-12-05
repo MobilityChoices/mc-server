9c64063e2594dc6878fef625214111e57432fde5
Florian Hämmerle <haemmerle.florian@googlemail.com>
2016-12-05 18:04:14 +0100

    add google geocode test fixture

---
+++ test/fixtures/googleGeocode.ts
@@ -0,0 +1,486 @@
+export default {
+  'results': [
+    {
+      'address_components': [
+        {
+          'long_name': '500',
+          'short_name': '500',
+          'types': ['street_number']
+        },
+        {
+          'long_name': 'Stadtstraße',
+          'short_name': 'Stadtstraße',
+          'types': ['route']
+        },
+        {
+          'long_name': 'Dornbirn',
+          'short_name': 'Dornbirn',
+          'types': ['locality', 'political']
+        },
+        {
+          'long_name': 'Dornbirn',
+          'short_name': 'Dornbirn',
+          'types': ['administrative_area_level_2', 'political']
+        },
+        {
+          'long_name': 'Vorarlberg',
+          'short_name': 'Vorarlberg',
+          'types': ['administrative_area_level_1', 'political']
+        },
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        },
+        {
+          'long_name': '6850',
+          'short_name': '6850',
+          'types': ['postal_code']
+        }
+      ],
+      'formatted_address': 'Stadtstraße 500, 6850 Dornbirn, Austria',
+      'geometry': {
+        'location': {
+          'lat': 47.4134281,
+          'lng': 9.744343299999999
+        },
+        'location_type': 'ROOFTOP',
+        'viewport': {
+          'northeast': {
+            'lat': 47.4147770802915,
+            'lng': 9.7456922802915
+          },
+          'southwest': {
+            'lat': 47.4120791197085,
+            'lng': 9.742994319708497
+          }
+        }
+      },
+      'place_id': 'ChIJMzWau05rm0cRk8VoN0Cud84',
+      'types': ['street_address']
+    },
+    {
+      'address_components': [
+        {
+          'long_name': 'Dornbirn Altes Rathaus',
+          'short_name': 'Dornbirn Altes Rathaus',
+          'types': ['establishment', 'point_of_interest']
+        },
+        {
+          'long_name': 'Dornbirn',
+          'short_name': 'Dornbirn',
+          'types': ['locality', 'political']
+        },
+        {
+          'long_name': 'Dornbirn',
+          'short_name': 'Dornbirn',
+          'types': ['administrative_area_level_2', 'political']
+        },
+        {
+          'long_name': 'Vorarlberg',
+          'short_name': 'Vorarlberg',
+          'types': ['administrative_area_level_1', 'political']
+        },
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        },
+        {
+          'long_name': '6850',
+          'short_name': '6850',
+          'types': ['postal_code']
+        }
+      ],
+      'formatted_address': 'Dornbirn Altes Rathaus, 6850 Dornbirn, Austria',
+      'geometry': {
+        'location': {
+          'lat': 47.412832,
+          'lng': 9.744413999999999
+        },
+        'location_type': 'APPROXIMATE',
+        'viewport': {
+          'northeast': {
+            'lat': 47.4141809802915,
+            'lng': 9.745762980291502
+          },
+          'southwest': {
+            'lat': 47.41148301970851,
+            'lng': 9.743065019708498
+          }
+        }
+      },
+      'place_id': 'ChIJuxIwUElrm0cRw4sbrt-JFYU',
+      'types': [
+        'bus_station',
+        'establishment',
+        'point_of_interest',
+        'transit_station'
+      ]
+    },
+    {
+      'address_components': [
+        {
+          'long_name': 'Dornbirn',
+          'short_name': 'Dornbirn',
+          'types': ['locality', 'political']
+        },
+        {
+          'long_name': 'Gemeinde Dornbirn',
+          'short_name': 'Gemeinde Dornbirn',
+          'types': ['administrative_area_level_3', 'political']
+        },
+        {
+          'long_name': 'Dornbirn District',
+          'short_name': 'Dornbirn District',
+          'types': ['administrative_area_level_2', 'political']
+        },
+        {
+          'long_name': 'Vorarlberg',
+          'short_name': 'Vorarlberg',
+          'types': ['administrative_area_level_1', 'political']
+        },
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        },
+        {
+          'long_name': '6850',
+          'short_name': '6850',
+          'types': ['postal_code']
+        }
+      ],
+      'formatted_address': '6850 Dornbirn, Austria',
+      'geometry': {
+        'bounds': {
+          'northeast': {
+            'lat': 47.44948000000001,
+            'lng': 9.848509999999999
+          },
+          'southwest': {
+            'lat': 47.28435,
+            'lng': 9.69253
+          }
+        },
+        'location': {
+          'lat': 47.4124,
+          'lng': 9.743789999999999
+        },
+        'location_type': 'APPROXIMATE',
+        'viewport': {
+          'northeast': {
+            'lat': 47.44948000000001,
+            'lng': 9.848509999999999
+          },
+          'southwest': {
+            'lat': 47.28435,
+            'lng': 9.69253
+          }
+        }
+      },
+      'place_id': 'ChIJ5d2ozfRqm0cRSd4ShVVtqgY',
+      'types': ['locality', 'political']
+    },
+    {
+      'address_components': [
+        {
+          'long_name': 'Gemeinde Dornbirn',
+          'short_name': 'Gemeinde Dornbirn',
+          'types': ['administrative_area_level_3', 'political']
+        },
+        {
+          'long_name': 'Dornbirn',
+          'short_name': 'Dornbirn',
+          'types': ['administrative_area_level_2', 'political']
+        },
+        {
+          'long_name': 'Vorarlberg',
+          'short_name': 'Vorarlberg',
+          'types': ['administrative_area_level_1', 'political']
+        },
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        },
+        {
+          'long_name': '6850',
+          'short_name': '6850',
+          'types': ['postal_code']
+        }
+      ],
+      'formatted_address': 'Gemeinde Dornbirn, 6850, Austria',
+      'geometry': {
+        'bounds': {
+          'northeast': {
+            'lat': 47.44948000000001,
+            'lng': 9.848509999999999
+          },
+          'southwest': {
+            'lat': 47.28435,
+            'lng': 9.69253
+          }
+        },
+        'location': {
+          'lat': 47.4124,
+          'lng': 9.743789999999999
+        },
+        'location_type': 'APPROXIMATE',
+        'viewport': {
+          'northeast': {
+            'lat': 47.44948000000001,
+            'lng': 9.848509999999999
+          },
+          'southwest': {
+            'lat': 47.28435,
+            'lng': 9.69253
+          }
+        }
+      },
+      'place_id': 'ChIJ5d2ozfRqm0cRrCHtJBrAwH4',
+      'types': ['administrative_area_level_3', 'political']
+    },
+    {
+      'address_components': [
+        {
+          'long_name': '6850',
+          'short_name': '6850',
+          'types': ['postal_code']
+        },
+        {
+          'long_name': 'Vorarlberg',
+          'short_name': 'Vorarlberg',
+          'types': ['administrative_area_level_1', 'political']
+        },
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        }
+      ],
+      'formatted_address': '6850, Austria',
+      'geometry': {
+        'bounds': {
+          'northeast': {
+            'lat': 47.4497371,
+            'lng': 9.8477844
+          },
+          'southwest': {
+            'lat': 47.2844006,
+            'lng': 9.691096999999999
+          }
+        },
+        'location': {
+          'lat': 47.4122675,
+          'lng': 9.7438375
+        },
+        'location_type': 'APPROXIMATE',
+        'viewport': {
+          'northeast': {
+            'lat': 47.4497371,
+            'lng': 9.8477844
+          },
+          'southwest': {
+            'lat': 47.2844006,
+            'lng': 9.691096999999999
+          }
+        }
+      },
+      'place_id': 'ChIJa-CGyvRqm0cRKvDMWJc_Ebo',
+      'types': ['postal_code']
+    },
+    {
+      'address_components': [
+        {
+          'long_name': 'Dornbirn',
+          'short_name': 'Dornbirn',
+          'types': ['postal_town']
+        },
+        {
+          'long_name': 'Dornbirn',
+          'short_name': 'Dornbirn',
+          'types': ['locality', 'political']
+        },
+        {
+          'long_name': 'Vorarlberg',
+          'short_name': 'Vorarlberg',
+          'types': ['administrative_area_level_1', 'political']
+        },
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        },
+        {
+          'long_name': '6850',
+          'short_name': '6850',
+          'types': ['postal_code']
+        }
+      ],
+      'formatted_address': 'Dornbirn, 6850 Dornbirn, Austria',
+      'geometry': {
+        'bounds': {
+          'northeast': {
+            'lat': 47.4497371,
+            'lng': 9.8477844
+          },
+          'southwest': {
+            'lat': 47.2844006,
+            'lng': 9.691096999999999
+          }
+        },
+        'location': {
+          'lat': 47.4070194,
+          'lng': 9.7603864
+        },
+        'location_type': 'APPROXIMATE',
+        'viewport': {
+          'northeast': {
+            'lat': 47.4497371,
+            'lng': 9.8477844
+          },
+          'southwest': {
+            'lat': 47.2844006,
+            'lng': 9.691096999999999
+          }
+        }
+      },
+      'place_id': 'ChIJa-CGyvRqm0cRWyHlfF3rq7k',
+      'types': ['postal_town']
+    },
+    {
+      'address_components': [
+        {
+          'long_name': 'Dornbirn District',
+          'short_name': 'Dornbirn District',
+          'types': ['administrative_area_level_2', 'political']
+        },
+        {
+          'long_name': 'Vorarlberg',
+          'short_name': 'Vorarlberg',
+          'types': ['administrative_area_level_1', 'political']
+        },
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        }
+      ],
+      'formatted_address': 'Dornbirn District, Austria',
+      'geometry': {
+        'bounds': {
+          'northeast': {
+            'lat': 47.4627601,
+            'lng': 9.848509999999999
+          },
+          'southwest': {
+            'lat': 47.28435,
+            'lng': 9.64439
+          }
+        },
+        'location': {
+          'lat': 47.4124,
+          'lng': 9.743789999999999
+        },
+        'location_type': 'APPROXIMATE',
+        'viewport': {
+          'northeast': {
+            'lat': 47.4627601,
+            'lng': 9.848509999999999
+          },
+          'southwest': {
+            'lat': 47.28435,
+            'lng': 9.64439
+          }
+        }
+      },
+      'place_id': 'ChIJqTi5idxqm0cRWsxg5N1hioo',
+      'types': ['administrative_area_level_2', 'political']
+    },
+    {
+      'address_components': [
+        {
+          'long_name': 'Vorarlberg',
+          'short_name': 'Vorarlberg',
+          'types': ['administrative_area_level_1', 'political']
+        },
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        }
+      ],
+      'formatted_address': 'Vorarlberg, Austria',
+      'geometry': {
+        'bounds': {
+          'northeast': {
+            'lat': 47.59621,
+            'lng': 10.23689
+          },
+          'southwest': {
+            'lat': 46.84081,
+            'lng': 9.530909899999999
+          }
+        },
+        'location': {
+          'lat': 47.2497427,
+          'lng': 9.9797373
+        },
+        'location_type': 'APPROXIMATE',
+        'viewport': {
+          'northeast': {
+            'lat': 47.59621,
+            'lng': 10.23689
+          },
+          'southwest': {
+            'lat': 46.84081,
+            'lng': 9.530910000000002
+          }
+        }
+      },
+      'place_id': 'ChIJRQmqIqESm0cRTZpV9Z8FT4Q',
+      'types': ['administrative_area_level_1', 'political']
+    },
+    {
+      'address_components': [
+        {
+          'long_name': 'Austria',
+          'short_name': 'AT',
+          'types': ['country', 'political']
+        }
+      ],
+      'formatted_address': 'Austria',
+      'geometry': {
+        'bounds': {
+          'northeast': {
+            'lat': 49.0206081,
+            'lng': 17.1606861
+          },
+          'southwest': {
+            'lat': 46.37233579999999,
+            'lng': 9.530783399999999
+          }
+        },
+        'location': {
+          'lat': 47.516231,
+          'lng': 14.550072
+        },
+        'location_type': 'APPROXIMATE',
+        'viewport': {
+          'northeast': {
+            'lat': 49.0205999,
+            'lng': 17.1606861
+          },
+          'southwest': {
+            'lat': 46.37233579999999,
+            'lng': 9.530783399999999
+          }
+        }
+      },
+      'place_id': 'ChIJfyqdJZsHbUcRr8Hk3XvUEhA',
+      'types': ['country', 'political']
+    }
+  ],
+  'status': 'OK'
+}
