c1c5b34ae55393a3cb0ad60720c6be58b48486f8
Florian Hämmerle <haemmerle.florian@googlemail.com>
2016-12-18 20:53:58 +0100

    add updateProfile method (route handler)

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -2,6 +2,7 @@ import { Request, IReply } from 'hapi'
 import { getAuthenticatedUser } from '../helpers/auth'
 import { sanitizeUser } from '../helpers/types'
 import { serverError, authenticationError } from '../helpers/errors'
+import UserRepository from '../services/storage/repositories/user'
 
 async function me(request: Request, reply: IReply) {
   try {
@@ -16,6 +17,21 @@ async function me(request: Request, reply: IReply) {
   }
 }
 
+async function updateProfile(request: Request, reply: IReply) {
+  try {
+    const user = await getAuthenticatedUser(request.headers['authorization'])
+    if (user) {
+      await UserRepository.update(user._id, request.payload)
+      return reply({ status: 'ok' }).code(200)
+    } else {
+      return reply({ error: authenticationError() }).code(401)
+    }
+  } catch (e) {
+    return reply({ error: serverError() }).code(500)
+  }
+}
+
 export default {
   me,
+  updateProfile,
 }
